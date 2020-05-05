189765bc5f19fcdacfd3b976787649ed3e6cd993
Shogo Ohta <athos0220@gmail.com>
2020-05-05 11:59:54 +0900

    first commit

---
+++ src/clojars_rss/core.clj
@@ -0,0 +1,58 @@
+(ns clojars-rss.core
+  (:require [clj-http.client :as http]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [pogonos.core :as pg]
+            [pogonos.output :as output])
+  (:import [java.text SimpleDateFormat]
+           [java.util Date Locale]))
+
+(defn- fetch-libs
+  ([since]
+   (fetch-libs since (System/currentTimeMillis)))
+  ([since until]
+   (let [query (format "at:[%d TO %d]" since until)
+         res (http/get "https://clojars.org/search"
+                       {:as :json
+                        :query-params {"q" query "format" "json"}})]
+     (->> (get-in res [:body :results])
+          (map (fn [lib] (update lib :created #(Long/parseLong %))))))))
+
+(defn- dump-libs [libs dump-file]
+  (with-open [w (io/writer dump-file)]
+    (binding [*out* w]
+      (doseq [lib libs]
+        (prn lib)))))
+
+(def ^:private format-date
+  (let [date-format (SimpleDateFormat. "EEE, d MMM yyyy HH:mm:ss Z" Locale/US)]
+    (fn [^Date d]
+      (.format date-format d))))
+
+(defn- ->feed-item [lib]
+  (let [name (cond->> (:jar_name lib)
+               (not= (:group_name lib) (:jar_name lib))
+               (str (:group_name lib) \/))
+        link (str "https://clojars.org/" name)]
+    {:guid (str link "/versions/" (:version lib))
+     :title name
+     :link link
+     :description (:description lib)
+     :publish-date (->> (:created lib)
+                        Date.
+                        format-date)}))
+
+(defn- generate-feed [libs build-date output-file]
+  (let [items (map ->feed-item libs)]
+    (pg/render-resource "feed_template.mustache"
+                        {:items items :last-build-date (format-date build-date)}
+                        {:output (output/to-file output-file)})))
+
+(defn -main [libs-in libs-out feed-file]
+  (let [old-libs (with-open [r (io/reader libs-in)]
+                   (mapv edn/read-string (line-seq r)))
+        libs (->> (fetch-libs (inc (:created (first old-libs))))
+                  (sort-by :created (comparator >)))
+        libs' (take 256 (concat libs old-libs))]
+    (dump-libs libs' libs-out)
+    (generate-feed libs' (Date.) feed-file)))
